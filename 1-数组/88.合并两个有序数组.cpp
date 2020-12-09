7a6e8c25fc4beb08f069d143cff0fd353a04731a
sikeyliu <sikeyliu647@163.com>
2020-12-09 11:40:46 +0800

    add 206

--- 1-数组/88.合并两个有序数组.cpp
+++ 1-数组/88.合并两个有序数组.cpp
@@ -4,11 +4,15 @@
  * [88] 合并两个有序数组
  */
 
-// @lc code=start
-
 #include <vector>
 using namespace std;
 
+// 方法：
+//  从后向前比较两个数组元素，较大者，移动到新数组对应的位置。
+//  使用 i,j 分别从 nums1[m-1] 和 nums2[n-1] 位置开始比较，
+//  取较大者，赋值给 nums[index]，并更新index。
+
+// @lc code=start
 class Solution {
    public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
--- 2-链表/206.反转链表.cpp
+++ 2-链表/206.反转链表.cpp
@@ -9,29 +9,48 @@
  * Definition for singly-linked list.
  */
 
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(nullptr) {}
-};
+// 方法一：（迭代）
+//  后一个节点的指针，指向前一个节点
+//  使用三个指针，pre记录前一个节点，cur记录当前节点，post后一个节点
+//  遍历链表，终止条件是 cur 不为空  
+//  
+//  空间复杂度：  
+//
+// 方法二：（递归）
+//  
+
+// struct ListNode {
+//     int val;
+//     ListNode* next;
+//     ListNode(int x) : val(x), next(nullptr) {}
+// };
 
 class Solution {
    public:
+    // 方法一：（迭代）
+    // ListNode* reverseList(ListNode* head) {
+    //     if (!head) return head;
+
+    //     ListNode *pre = nullptr, *cur = head, *post = nullptr;
+
+    //     while (cur) {
+    //         post = cur->next;
+    //         cur->next = pre;
+    //         pre = cur;
+    //         cur = post;
+    //     }
+
+    //     return pre;
+    // }
+
+    // 方法二：（递归）
     ListNode* reverseList(ListNode* head) {
-        /*if (head == nullptr || head->next == nullptr) return head;
-        ListNode *pre = nullptr, *cur = head, *tmp = nullptr;
-        while (cur != nullptr) {
-            tmp = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = tmp;
-        }
-        return pre;*/
-        if (head == nullptr || head->next == nullptr) return head;
-        ListNode* p = reverseList(head->next);
+        if (!head || !head->next) return head;
+
+        ListNode *h = reverseList(head->next);
         head->next->next = head;
         head->next = nullptr;
-        return p;
+        return h;
     }
 };
 // @lc code=end
