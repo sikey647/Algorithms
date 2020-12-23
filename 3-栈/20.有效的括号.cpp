3a6c66f7ad6b03d061aebaaa4bec3487e7237809
sikey64 <sikeyliu647@163.com>
2020-12-23 08:29:16 +0800

    add code

--- 3-栈/20.有效的括号.cpp
+++ 3-栈/20.有效的括号.cpp
@@ -14,7 +14,8 @@ using namespace std;
 // 思路：
 //  使用栈
 //  遍历字符串，字符为左括号，则入栈；
-//  如果字符为右括号，则与栈顶元素比较，如果匹配则出栈，则继续下一个字符
+//  如果字符为右括号，则与栈顶元素比较，如果匹配则出栈，则继续下一个字符；
+//  如果不匹配或栈为空，则说明为无效括号
 //  当遍历完成后，如果栈为空，则说明为有效括号，如果非空，为无效括号
 
 class Solution {
@@ -25,13 +26,13 @@ class Solution {
             {')', '('}, {'}', '{'}, {']', '['}};
 
         for (char c : s) {
-            if (charMap.find(c) != charMap.end()) {
+            if (charMap.find(c) != charMap.end()) {  // 左括号
                 if (charStack.empty() || charMap[c] != charStack.top()) {
                     return false;
                 } else {
                     charStack.pop();
                 }
-            } else {
+            } else {  // 右括号
                 charStack.push(c);
             }
         }
