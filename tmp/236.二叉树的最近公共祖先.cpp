560dc5ea345f7e4c44597b400c5bbbe49aee1240
sikey64 <sikeyliu647@163.com>
2021-08-12 08:35:05 +0800

    add common

--- tmp/236.二叉树的最近公共祖先.cpp
+++ tmp/236.二叉树的最近公共祖先.cpp
@@ -7,18 +7,34 @@
 // @lc code=start
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
  */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
 class Solution {
-public:
+   public:
+    // root 是 p、q 的最近的公共祖先，只能为以下情况
+    //  p 和 q 在 root 的子树中，且分别在 root 的异侧
+    //  p = root, 且 q 在 root 的子树中
+    //  q = root, 且 p 在 root 的子树中
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        // 递归终止条件
+        if (root == nullptr || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left && right) {
+            return root;
+        }
+
+        return left ? left : right;
     }
 };
 // @lc code=end
-
